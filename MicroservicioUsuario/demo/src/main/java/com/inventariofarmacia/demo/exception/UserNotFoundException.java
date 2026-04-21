e24450e0acc0d9cdee5dfb58d31c725b25a189f6
Tiare Salazar <tiar.salazar@duocuc.cl>
2026-04-20 22:35:38 -0400

    elimine archivos redundantes

--- MicroservicioUsuario/demo/src/main/java/com/inventariofarmacia/demo/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.inventariofarmacia.demo.exception;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(String msj){
-        super(msj);
-    }
-
-}
