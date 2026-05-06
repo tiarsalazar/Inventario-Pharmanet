b2c18a0192e62625725c339e199736c303c9dab6
Tiare Salazar <tiar.salazar@duocuc.cl>
2026-05-06 01:09:32 -0400

    se agrega wallet de oracle, se modifica name 'sucursal_service' por 'sucursal-service' para conectar feign y se modifican properties

--- usuario_service/src/main/java/com/pharmanet/usuario_service/client/SucursalFeignClient.java
+++ usuario_service/src/main/java/com/pharmanet/usuario_service/client/SucursalFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.pharmanet.usuario_service.dto.SucursalDTO;
 
-@FeignClient(name = "sucursal_service")
+@FeignClient(name = "sucursal-service")
 public interface SucursalFeignClient {
 
     @GetMapping("/{codInterno}")
