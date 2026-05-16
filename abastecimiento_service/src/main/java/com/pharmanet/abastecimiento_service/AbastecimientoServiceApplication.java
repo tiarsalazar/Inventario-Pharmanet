b1345faa04ccb0a5aaac1f0695c0180458990e27
ArielDsz <ar.nunezd@duocuc.cl>
2026-05-15 21:00:48 -0400

    Base microservicios inventario y abastecimiento

---
+++ abastecimiento_service/src/main/java/com/pharmanet/abastecimiento_service/AbastecimientoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pharmanet.abastecimiento_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbastecimientoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbastecimientoServiceApplication.class, args);
+	}
+
+}
---
+++ abastecimiento_service/src/test/java/com/pharmanet/abastecimiento_service/AbastecimientoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmanet.abastecimiento_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbastecimientoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
