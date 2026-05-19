ed6265bc38a248463248fe3152304b372a2647b8
ArielDsz <ar.nunezd@duocuc.cl>
2026-05-19 17:36:01 -0400

    feat: agrega script V1 create tables y anotacion enableFeignClient

--- abastecimiento_service/src/main/java/com/pharmanet/abastecimiento_service/AbastecimientoServiceApplication.java
+++ abastecimiento_service/src/main/java/com/pharmanet/abastecimiento_service/AbastecimientoServiceApplication.java
@@ -2,8 +2,10 @@ package com.pharmanet.abastecimiento_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AbastecimientoServiceApplication {
 
 	public static void main(String[] args) {
