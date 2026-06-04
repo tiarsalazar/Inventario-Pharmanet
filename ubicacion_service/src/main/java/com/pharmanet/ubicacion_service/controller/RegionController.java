a941b11ab0e4149431cdf403664a94f5f1d8acb4
VI-Alumno <VI-Alumno@duoc.acad>
2026-06-04 17:49:13 -0400

    se implementa swagger: RegionController

--- ubicacion_service/src/main/java/com/pharmanet/ubicacion_service/controller/RegionController.java
+++ ubicacion_service/src/main/java/com/pharmanet/ubicacion_service/controller/RegionController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pharmanet.ubicacion_service.dto.RegionDto;
 import com.pharmanet.ubicacion_service.service.RegionService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,11 +31,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 @RequestMapping("/api/v1/regiones")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "Regiones", description = "Operaciones relacionadas con las regiones.")
 public class RegionController {
 
     private final RegionService regionService;
 
     @GetMapping
+    @Operation(summary = "Obtiene todas las regiones", description = "Obtiene un pageable de todas las regiones")
+    @ApiResponse(responseCode = "200", description = "Se obtiene una lista de todos los pageables")
     public ResponseEntity<Page<RegionDto>> mostrarTodos(@PageableDefault(size = 10, sort = "codRegion") Pageable pageable) {
         log.info("Inicia búsqueda de todas las regiones.");
         Page<RegionDto> regiones = regionService.mostrarTodasRegiones(pageable);
@@ -38,24 +46,47 @@ public class RegionController {
     }
 
     @GetMapping("/{codRegion}")
+    @Operation(summary = "Busca una región", description = "Busca una región por código de región")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Obtiene una región exitosamente"),
+        @ApiResponse(responseCode = "404", description = "No encuentra la región con el código ingresado")
+    })
+    @Parameter(name = "codRegion", description = "Código de la region", required = true)
     public ResponseEntity<RegionDto> buscarRegion(@PathVariable String codRegion) {
         RegionDto region = regionService.buscarRegion(codRegion);
         return ResponseEntity.status(HttpStatus.OK).body(region);
     }
 
     @PostMapping
-    public ResponseEntity<RegionDto> agregarRegion(@Valid @RequestBody RegionDto region) {
+    @Operation(summary = "Agrega una región", description = "Agrega una región nueva")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Agrega una región exitosamente"),
+        @ApiResponse(responseCode = "400", description = "Fallo en las validaciones")
+    })
+    public ResponseEntity<RegionDto> agregarRegion(@Valid @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Región a crear", required = true) @RequestBody RegionDto region) {
         RegionDto resultado = regionService.agregarRegion(region);
         return ResponseEntity.status(HttpStatus.CREATED).body(resultado);
     }
     
+    @Operation(summary = "Actualiza una región")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "Actualiza una región exitosamente"),
+        @ApiResponse(responseCode = "404", description = "No se encuentra la región a actualizar"),
+        @ApiResponse(responseCode = "400", description = "Fallo en las validaciones")
+    })
     @PutMapping
-    public ResponseEntity<?> actualizarRegion(@Valid @RequestBody RegionDto region) {
+    public ResponseEntity<?> actualizarRegion(@Valid @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Región a actualizar", required = true) @RequestBody RegionDto region) {
         regionService.actualizarRegion(region);   
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @Operation(summary = "Elimina una región", description = "Elimina una región por código ingresado")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "Se elimina una región exitosamente"),
+        @ApiResponse(responseCode = "404", description = "No se encuentra la región con el código ingresado")
+    })
     @DeleteMapping("/{codRegion}")
+    @Parameter(name = "codRegion", description = "Código de la región a eliminar", required = true)
     public ResponseEntity<?> eliminarRegion(@PathVariable String codRegion) {
         regionService.eliminarRegion(codRegion);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
