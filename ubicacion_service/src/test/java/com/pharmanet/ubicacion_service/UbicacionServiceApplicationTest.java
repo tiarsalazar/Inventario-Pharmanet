9be0af009b45862075491ee98a58f8bc8d01067c
VI-Alumno <VI-Alumno@duoc.acad>
2026-06-22 18:29:25 -0400

    fix: arreglar Application Test

---
+++ ubicacion_service/src/test/java/com/pharmanet/ubicacion_service/UbicacionServiceApplicationTest.java
@@ -0,0 +1,15 @@
+package com.pharmanet.ubicacion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class UbicacionServiceApplicationTest {
+    
+    @Test
+    void contextLoads() {
+
+    }
+}
